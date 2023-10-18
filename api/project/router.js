c52978ccace8748432164942d39bf8c6aec7fbe9
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-10-18 15:30:15 -0700

    server running

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,14 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+// const model = require('./model')
+
+
+router.use((err, req, res,) => {
+    res.status(500).json({
+        customMessage: 'something went wrong',
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // start your server here
+require('dotenv').config();
+
+const server = require('./api/server');
+
+const port = process.env.PORT || 7000
+
+server.listen(port, () => {
+     console.log(`server running on port ${port}`)
+});
