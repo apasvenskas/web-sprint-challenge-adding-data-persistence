93da2788ac9ec6285e2b241ba1c332fd4544801e
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-10-23 23:42:25 -0700

    post resources functioning

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,6 +1,7 @@
 // build your `/api/resources` router here
 const express = require('express');
 const { getResources, addResource } = require('./model');
+const { resource } = require('../server');
 const appResource = express.Router();
 
 appResource.use(express.json());
@@ -19,16 +20,21 @@ appResource.get('/', (req, res) => {
 
 appResource.post('/', (req, res) => {
     let resourceName = req.body.resource_name; 
+    let resourceDescription = req.body.resource_description
     if (!resourceName) {
         return res.status(400).json({error: 'Rsource name is required'});
     }
     let newResource = {
-        resource_id: addResource.length + 1,
+        // resource_id: addResource.length + 1,
         resource_name: resourceName,
-        resource_description: req.body.resource_description || null
+        resource_description: resourceDescription || null
     }
-    addResource.push(newResource);
-    res.status(201).json(newResource);
+    addResource(newResource)
+    .then(resource => {
+        res.status(201).json(resource)
+    }).catch(error => {
+        res.status(500).json({error: error.message})
+    })
 });
 
 
