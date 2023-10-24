f2c3b636910b5f233eea2a6961ee94ad6b718e6f
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-10-24 15:14:39 -0700

    tasks rendering

--- api/task/model.js
+++ api/task/model.js
@@ -1,21 +1,32 @@
 // build your `Task` model here
-const db = require('../../data/dbConfig');
+const db = require("../../data/dbConfig");
 
-function getAll() {
-    return db('task');
+function getTasks() {
+    console.log(db('tasks'));
+  return db("tasks")
+    .join("projects", "tasks.project_id", "projects.project_id")
+    .select([
+      "task_id",
+      "task_description",
+      "task_notes",
+      "task_completed",
+      "tasks.project_id as task_project_id",
+      "project_name",
+      "project_description",
+    ]);
 }
 
-function getById(id) {
-    return db('task').where({id}).first();
+function getByTaskId(id) {
+  return db("task").where({ id }).first();
 }
 
-async function add(task) {
-    const[id] = await db('task').insert(task);
-    return getById(id);
+async function addTask(task) {
+  const [id] = await db("task").insert(task);
+  return addTask(id);
 }
 
 module.exports = {
-    add,
-    getAll,
-    getById
-}
+  getTasks,
+  getByTaskId,
+  addTask,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,38 +1,52 @@
 // build your `/api/tasks` router here
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const Task = require('./model');
-const Project = require('../project/model')
+const { addTask, getTasks, getByTaskId } = require("./model");
+// const Project = require("../project/model");
 
-router.post('/api/tasks', async (req, res) => {
-    const { task_description, task_notes, task_completed, project_id } = req.body;
-    if (!task_description || !project_id){
-        return res.status(400).json({message: 'Please provide task_description and project_id'});
-    }
-    const completed = task_completed ? 1:0;
-    try {
-        const newTask = await Task.add({task_description, task_notes, completed, project_id})
-        res.status(201).json({...newTask, task_completed: !!newTask.task_completed});
-    } catch (err) {
-        res.status(500).json({message: 'Failed to create new task'});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await getTasks();
+    console.log(tasks);
+    const taskWithProjectInfo = await Promise.all(
+      tasks.map(async (task) => {
+        return {
+          ...task,
+          project_name: task.project_name,
+          project_description: task.project_description,
+          task_completed: Boolean(task.task_completed),
+        };
+      })
+    );
+    console.log(taskWithProjectInfo)
+    res.status(200).json(taskWithProjectInfo);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get tasks" });
+  }
 });
 
-router.get('/api/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.getAll();
-        const taskWithProjectInfo = await Promise.all(
-            tasks.map(async (task) => {
-                const project = await Project.getById(task.project_id);
-                return {...task, project_name: project.project_name, project_description: 
-                    project.project_description, task_completed: !!task.task_completed};
-            })
-        )
-        res.status(200).json(taskWithProjectInfo);
-    } catch(err){
-        res.status(500).json({message: 'Failed to get tasks'});
-    }
-})
+router.post("/", async (req, res) => {
+  const { task_description, task_notes, task_completed, project_id } = req.body;
+  if (!task_description || !project_id) {
+    return res
+      .status(400)
+      .json({ message: "Please provide task_description and project_id" });
+  }
+  const completed = task_completed ? 1 : 0;
+  try {
+    const newTask = await addTask.add({
+      task_description,
+      task_notes,
+      completed,
+      project_id,
+    });
+    res
+      .status(201)
+      .json({ ...newTask, task_completed: !!newTask.task_completed });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to create new task" });
+  }
+});
 
 module.exports = router;
